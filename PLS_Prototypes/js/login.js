8c3b85f0488860b94f0ce3a28ea54106a2de5533
Jordan Hinson <jordan.hinson@g.fmarion.edu>
2022-09-19 22:03:40 -0400

    Tweaked ui a bit

--- PLS_Prototypes/js/login.js
+++ PLS_Prototypes/js/login.js
@@ -1,19 +1,15 @@
 function signinclick() {
 
+    console.log("anuthing");
+
     let user = {
         username: document.getElementById("username_field").value,
         password: document.getElementById("password_field").value
     }
 
-    if(user.username.length === 0 ) {
-        console.log("invalid user");
-        $("#username_fieldr").css("border", "red")
+    if(!(user.username === "admin" && user.password === "password")) {
+        $("invalid_login_prompt").prop("display","inline");
     }
-
-    if(user.password.length === 0) {
-        console.log(user.password);
-    }
-
     else {
         console.log("valid user");
     }
