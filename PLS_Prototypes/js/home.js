09dcec9abe9ca4f2e187e34b207099c86a55b30d
Jordan Hinson <jordan.hinson@g.fmarion.edu>
2022-09-24 21:48:37 -0400

    Started testing out bootstrap for the home page use

--- PLS_Prototypes/js/home.js
+++ PLS_Prototypes/js/home.js
@@ -11,6 +11,19 @@ function timeClock() {
     }
 
     date += now.getMinutes().toString().padStart(2,"0") + ":" + now.getSeconds().toString().padStart(2,"0");
-
     $("#clock_numbers").html(date);
+}
+
+function setDates() {
+
+    let date = new Date();
+    let day_id = "#day_";
+
+    for(let i = 0; i < 14; i++) {
+        date.setDate(date.getDate() + i);
+        day_id = day_id.substring(0, day_id.lastIndexOf("_") + 1);
+        day_id += i;
+        $(day_id).html(date.toDateString());
+    }
+
 }
