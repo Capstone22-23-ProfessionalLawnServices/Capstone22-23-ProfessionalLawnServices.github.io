b12c1aa34b392d7e4145248448ad598c1e46002d
Jordan Hinson <jhinson2012@gmail.com>
2022-10-02 12:40:38 -0400

    Made clock buttons show and hide

--- PLS_Prototypes/js/home.js
+++ PLS_Prototypes/js/home.js
@@ -154,3 +154,47 @@ function getDateColors(date) {
 
     return dateColors;
 }
+
+function startSession() {
+    $("#start-session-button").hide();
+    $("#resume-session-button").hide();
+    $("#client-list").hide();
+    $("#client-list-label").hide();
+    $("#pause-session-button").show();
+    $("#end-session-button").show();
+    $("#active-client").show();
+    $("#active-client-label").show();
+}
+
+function pauseSession() {
+    $("#pause-session-button").hide();
+    $("#start-session-button").hide();
+    $("#client-list").hide();
+    $("#client-list-label").hide();
+    $("#resume-session-button").show();
+    $("#end-session-button").show();
+    $("#active-client").show();
+    $("#active-client-label").show();
+}
+
+function resumeSession() {
+    $("#resume-session-button").hide();
+    $("#start-session-button").hide();
+    $("#client-list").hide();
+    $("#client-list-label").hide();
+    $("#pause-session-button").show();
+    $("#end-session-button").show();
+    $("#active-client").show();
+    $("#active-client-label").show();
+}
+
+function endSession() {
+    $("#end-session-button").hide();
+    $("#pause-session-button").hide();
+    $("#active-client").hide();
+    $("#active-client-label").hide();
+    $("#resume-session-button").hide();
+    $("#start-session-button").show();
+    $("#client-list").show();
+    $("#client-list-label").show();
+}
