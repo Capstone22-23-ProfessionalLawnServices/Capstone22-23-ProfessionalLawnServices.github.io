d08407fdfc9cf2206a54001cc60c8ccf040dc7d5
Jordan Hinson <jhinson2012@gmail.com>
2022-11-15 10:55:40 -0500

    Added appointments template and links. Made common css and js files

--- www/modules/home/home.js
+++ www/modules/home/home.js
@@ -297,18 +297,3 @@ function endSession() {
     $("#client-list").show();
     $("#client-list-label").show();
 }
-
-function navbarDropdown(e) {
-
-    let button_id = "#" + e.target.id;
-    let dropdown_display_id = $(button_id).parent().next();
-
-    if ($(dropdown_display_id).css("display") === "none") {
-        $(button_id).prop("src", "../../../images/caret-down-square.svg")
-        $(dropdown_display_id).css("display", "block")
-    }
-    else {
-        $(button_id).prop("src", "../../../images/caret-right-square.svg")
-        $(dropdown_display_id).css("display", "none")
-    }
-}
